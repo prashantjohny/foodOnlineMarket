c07125f2923b6fbe11f63ecbee1c74b173b2a816
prashantjohny <prasp3677@gmail.com>
2025-07-25 22:13:32 +0530

    vendor and customer dashboard edits

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.views.decorators.csrf import csrf_protect
+from vendor.models import Vendor
 # Create your views here.
 
 # Restrict thr vendor from accessing the customer page
