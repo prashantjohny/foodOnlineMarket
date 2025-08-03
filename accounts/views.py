919479ba8b4423bb2db60a2366d9a1b5a9d044a9
prashantjohny <prasp3677@gmail.com>
2025-08-03 21:18:09 +0530

    cart functionality enable at frontend and backend and also using ajax to real time application

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.views.decorators.csrf import csrf_protect
 from vendor.models import Vendor
+from django.template.defaultfilters import slugify
 # Create your views here.
 
 # Restrict thr vendor from accessing the customer page
@@ -84,6 +85,8 @@ def registerVendor(request):
             user.save()
             vendor=v_form.save(commit=False)
             vendor.user=user
+            vendor_name=v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug=slugify(vendor_name)+'-'+str(user.id)
             user_profile=UserProfile.objects.get(user=user)
             vendor.user_profile=user_profile
             vendor.save()
