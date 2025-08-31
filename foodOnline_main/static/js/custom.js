c6696284241c6386a9a32603967314d3e5f1a9c4
prashantjohny <prasp3677@gmail.com>
2025-08-31 21:59:54 +0530

    dynamic tax calculating and showing at frontend

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -92,10 +92,11 @@ $(document).ready(function(){
                     //subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                         
                     )
+                    // console.log(response.cart_amount['tax_dict']);
                 }
             }
         })
@@ -137,7 +138,7 @@ $(document).ready(function(){
                     //subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                         
                     )
@@ -174,7 +175,7 @@ $(document).ready(function(){
                     //subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                         
                     )
@@ -204,11 +205,16 @@ $(document).ready(function(){
     }
 
     //apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname=='/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
+            
             $('#total').html(grand_total)
+            for(key1 in tax_dict){
+                for(key2 in tax_dict[key1]){
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
