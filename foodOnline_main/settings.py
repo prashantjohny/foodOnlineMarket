21c98152653cf47b541ad2822b184494de924c72
prashantjohny <prasp3677@gmail.com>
2025-09-06 17:19:08 +0530

    orders and order_detail functionalities - customer/vendor and calculating the total revenue and orders to demonstrate on the fronted of website and also implementing datatable plugin for pagination and realtime order

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "orders.request_object.RequestObjectMiddleware", # custom middleware to access the request object in model.py
 ]
 
 ROOT_URLCONF = "foodOnline_main.urls"
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,HttpResponse
-from marketplace.models import Cart
+from marketplace.models import Cart,Tax
 from marketplace.context_processors import get_cart_amounts
 from .forms import OrderForm
 from .models import Order,Payment,OrderedFood
@@ -8,6 +8,7 @@ from .utils import generate_order_number
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from menu.models import FoodItem
 import razorpay
 # from foodOnline_main.settings import RZP_KEY_ID,RZP_KEY_SECRET
 
@@ -21,6 +22,40 @@ def place_order(request):
     cart_count=cart_item.count()
     if cart_count<=0:
         return redirect('marketplace')
+    
+    vendors_ids=[]
+    for i in cart_item:
+        if i.fooditem.vendor.id not in vendors_ids:
+            vendors_ids.append(i.fooditem.vendor.id)
+    
+    ## from here it is calculating total payment for individual vendors ---->
+    get_tax=Tax.objects.filter(is_active=True)
+    subtotal=0
+    total_data={}
+    k={} 
+    for i in cart_item:
+        fooditem=FoodItem.objects.get(pk=i.fooditem.id, vendor_id__in=vendors_ids)
+        v_id=fooditem.vendor.id
+        if v_id in k:
+            subtotal=k[v_id]
+            subtotal += (fooditem.price * i.quantity)
+            k[v_id]=subtotal
+        else:
+            subtotal=(fooditem.price * i.quantity)
+            k[v_id]=subtotal
+    
+        # calculate the tax_data
+        tax_dict={}
+        for i in get_tax:
+            tax_type=i.tax_type
+            tax_percentage=i.tax_percentage
+            tax_amount=round((tax_percentage * subtotal)/100,2)
+            tax_dict.update({tax_type:{str(tax_percentage):str(tax_amount)}})
+        # construct total data
+        total_data.update({fooditem.vendor.id: {str(subtotal): str(tax_dict)}})
+        
+    # <----- to here
+    
     subtotal=get_cart_amounts(request)['subtotal']
     total_tax=get_cart_amounts(request)['tax']
     grand_total=get_cart_amounts(request)['grand_total']
@@ -42,10 +77,12 @@ def place_order(request):
             order.user=request.user
             order.total=grand_total
             order.tax_data=json.dumps(tax_data)
+            order.total_data=json.dumps(total_data)
             order.total_tax=total_tax
             order.payment_method=request.POST['payment_method']
             order.save()
             order.order_number=generate_order_number(order.id)
+            order.vendors.add(*vendors_ids)
             order.save()
             
             # Razorpay Payment
